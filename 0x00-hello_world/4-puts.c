4081a0447acc67b417301a66fa486688bcd2e7fc
ezirimobinna <ezirimobinna@gmail.com>
2022-07-09 02:48:05 -0700

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/
- * main - Entry point
-* Return: Always 0 (sucess)
-*/
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
